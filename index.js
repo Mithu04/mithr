c04b5f969f2a70be2bec9c3000a6de250fc289bb
Mithu04 <mithradevidv@gmail.com>
2026-02-16 11:03:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let btnElement = document.getElementById("btnElement");
+let imageContainerEl = document.getElementById("imageContainer");
+
+btnElement.addEventListener("click", function(){
+    imageContainerEl.classList.toggle("image-container-1");
+});
